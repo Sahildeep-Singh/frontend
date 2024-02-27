ff2b537d562e14d1838519ee2059283d5f3c0d62
Sahildeep-singh <420sahildeepsingh@gmail.com>
2024-02-27 20:32:12 +0530

    change/config

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 exports.API = {
-  API_BASE_URL: "https://backend-hnyn.onrender.com/api/v1/",
+  API_BASE_URL: "https://backend-1-zf5l.onrender.com/api/v1/",
   LOGIN: "user/login",
   REGISTER: "user/register",
   USERS: "user/users",
